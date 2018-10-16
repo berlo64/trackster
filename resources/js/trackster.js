6895d3b3e93a88c7ae20399ab5cf81fe14a44262
root <root@DESKTOP-3PBMK8U.localdomain>
2018-10-16 03:09:29 +0200

    add functionality for dynamic request

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
-$('#search-button').click(function() {
+const API_KEY = '10cfca331864a8701bf08a33d7a722cb';
+
+var Trackster = $('#search-button').click(function() {
   Trackster.searchTracksByTitle($('#search-box').val());
 });
-
-var Trackster = {};
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks.
@@ -18,6 +18,12 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-
+  $.ajax({
+    url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
+    success: function(results) {
+      console.log(results);
+    }
+  });
 };
+
 });
